b7504352b3ac4d4c13c1267625ee172fba6af738
AloyseTech <meyertheopro@gmail.com>
2019-09-25 16:35:01 +0200

    start working on I2C driver

--- include/lis3dh.h
+++ include/lis3dh.h
@@ -1,5 +1,11 @@
 #ifndef __LIS3DH_C
 #define __LIS3DH_C
+#include "esp_system.h"
 
+#if CONFIG_LIS3DH_INTERFACE_SPI
+#error "SPI interface for LIS3DH is not yet supported"
+#endif
+
+esp_err_t LIS3DH_Init();
 
 #endif //__LIS3DH_C
--- lis3dh.c
+++ lis3dh.c
@@ -1,1 +1,76 @@
 #include "lis3dh.h"
+#include "esp_system.h"
+#include "driver/i2c.h"
+#include "freertos/FreeRTOS.h"
+
+//For VS Code linter
+#include "../../build/config/sdkconfig.h"
+
+#define _I2C_NUMBER(num) I2C_NUM_##num
+#define I2C_NUMBER(num) _I2C_NUMBER(num)
+
+static esp_err_t i2c_master_init(void)
+{
+    int i2c_master_port = I2C_NUMBER(CONFIG_LIS3DH_I2C_PERIPH_NUM);
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = CONFIG_LIS3DH_I2C_SDA_PIN;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = CONFIG_LIS3DH_I2C_SCL_PIN;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = CONFIG_LIS3DH_I2C_SPEED;
+    i2c_param_config(i2c_master_port, &conf);
+    return i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0);
+}
+
+/**
+ * @brief Test code to write esp-i2c-slave
+ *        Master device write data to slave(both esp32),
+ *        the data will be stored in slave buffer.
+ *        We can read them out from slave buffer.
+ *
+ * ___________________________________________________________________
+ * | start | slave_addr + wr_bit + ack | write n bytes + ack  | stop |
+ * --------|---------------------------|----------------------|------|
+ *
+ */
+esp_err_t LIS3DH_WriteReg(uint8_t reg, uint8_t value)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (CONFIG_LIS3DH_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, 1);
+    i2c_master_write_byte(cmd, reg, 0);
+    i2c_master_write_byte(cmd, value, 1);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(CONFIG_LIS3DH_I2C_PERIPH_NUM, cmd, pdMS_TO_TICKS(100));
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+/**
+ * @brief test code to read esp-i2c-slave
+ *        We need to fill the buffer of esp slave device, then master can read them out.
+ *
+ * _______________________________________________________________________________________
+ * | start | slave_addr + rd_bit +ack | read n-1 bytes + ack | read 1 byte + nack | stop |
+ * --------|--------------------------|----------------------|--------------------|------|
+ *
+ */
+esp_err_t LIS3DH_ReadReg(uint8_t reg, uint8_t *dataRead)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (CONFIG_LIS3DH_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, 1);
+    i2c_master_write_byte(cmd, reg, 1);
+    i2c_master_read_byte(cmd, dataRead, 1);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(CONFIG_LIS3DH_I2C_PERIPH_NUM, cmd, pdMS_TO_TICKS(100));
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+esp_err_t LIS3DH_Init()
+{
+    esp_err_t err = i2c_master_init();
+    return err;
+}
