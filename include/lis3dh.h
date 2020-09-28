e459c7d41dcbb1cf81aab758fa85be8d446599af
AloyseTech <meyertheopro@gmail.com>
2020-09-28 23:08:57 +0200

    add motion detection WIP

--- include/lis3dh.h
+++ include/lis3dh.h
@@ -6,6 +6,58 @@
 #error "SPI interface for LIS3DH is not yet supported"
 #endif
 
+// List of LIS3DH registers
+#define LIS3DH_REG_STATUS_AUX   0x07
+#define LIS3DH_REG_OUT_ADC1_L   0x08
+#define LIS3DH_REG_OUT_ADC1_H   0x09
+#define LIS3DH_REG_OUT_ADC2_L   0x0a
+#define LIS3DH_REG_OUT_ADC2_H   0x0b
+#define LIS3DH_REG_OUT_ADC3_L   0x0c
+#define LIS3DH_REG_OUT_ADC3_H   0x0d
+#define LIS3DH_REG_INT_COUNTER  0x0e
+#define LIS3DH_REG_WHO_AM_I     0x0f
+#define LIS3DH_REG_TEMP_CFG     0x1f
+#define LIS3DH_REG_CTRL1        0x20
+#define LIS3DH_REG_CTRL2        0x21
+#define LIS3DH_REG_CTRL3        0x22
+#define LIS3DH_REG_CTRL4        0x23
+#define LIS3DH_REG_CTRL5        0x24
+#define LIS3DH_REG_CTRL6        0x25
+#define LIS3DH_REG_REFERENCE    0x26
+#define LIS3DH_REG_STATUS       0x27
+#define LIS3DH_REG_OUT_X_L      0x28
+#define LIS3DH_REG_OUT_X_H      0x29
+#define LIS3DH_REG_OUT_Y_L      0x2a
+#define LIS3DH_REG_OUT_Y_H      0x2b
+#define LIS3DH_REG_OUT_Z_L      0x2c
+#define LIS3DH_REG_OUT_Z_H      0x2d
+#define LIS3DH_REG_FIFO_CTRL    0x2e
+#define LIS3DH_REG_FIFO_SRC     0x2f
+#define LIS3DH_REG_INT1_CFG     0x30
+#define LIS3DH_REG_INT1_SRC     0x31
+#define LIS3DH_REG_INT1_THS     0x32
+#define LIS3DH_REG_INT1_DUR     0x33
+#define LIS3DH_REG_INT2_CFG     0x34
+#define LIS3DH_REG_INT2_SRC     0x35
+#define LIS3DH_REG_INT2_THS     0x36
+#define LIS3DH_REG_INT2_DUR     0x37
+#define LIS3DH_REG_CLICK_CFG    0x38
+#define LIS3DH_REG_CLICK_SRC    0x39
+#define LIS3DH_REG_CLICK_THS    0x3a
+#define LIS3DH_REG_TIME_LIMIT   0x3b
+#define LIS3DH_REG_TIME_LATENCY 0x3c
+#define LIS3DH_REG_TIME_WINDOW  0x3d
+
+typedef struct
+{
+    uint8_t highPassFilterCutoff;
+    uint8_t highPassFilterMode;
+    uint8_t ODR;
+} LIS3DH_IRQ_Config_t;
+
 esp_err_t LIS3DH_Init();
+esp_err_t LIS3DH_ReadReg(uint8_t reg, uint8_t *readData);
+esp_err_t LIS3DH_ConfigureMotionDetection(uint8_t ODR, uint8_t LPen, uint8_t axisEn, uint8_t enIrqPin1, uint8_t scale,
+                                          uint8_t latch, uint8_t D4D, uint8_t threshold, uint8_t duration);
 
 #endif //__LIS3DH_C
--- lis3dh.c
+++ lis3dh.c
@@ -1,23 +1,26 @@
 #include "lis3dh.h"
-#include "esp_system.h"
 #include "driver/i2c.h"
+#include "esp_log.h"
+#include "esp_system.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
 
-//For VS Code linter
+// For VS Code linter
 #include "../../build/config/sdkconfig.h"
 
 #define _I2C_NUMBER(num) I2C_NUM_##num
-#define I2C_NUMBER(num) _I2C_NUMBER(num)
+#define I2C_NUMBER(num)  _I2C_NUMBER(num)
 
-static esp_err_t i2c_master_init(void)
+esp_err_t LIS3DH_I2C_Init(void)
 {
-    int i2c_master_port = I2C_NUMBER(CONFIG_LIS3DH_I2C_PERIPH_NUM);
+    int          i2c_master_port = I2C_NUMBER(CONFIG_LIS3DH_I2C_PERIPH_NUM);
     i2c_config_t conf;
-    conf.mode = I2C_MODE_MASTER;
-    conf.sda_io_num = CONFIG_LIS3DH_I2C_SDA_PIN;
-    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-    conf.scl_io_num = CONFIG_LIS3DH_I2C_SCL_PIN;
-    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.mode             = I2C_MODE_MASTER;
+    conf.sda_io_num       = CONFIG_LIS3DH_I2C_SDA_PIN;
+    conf.sda_pullup_en    = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num       = CONFIG_LIS3DH_I2C_SCL_PIN;
+    conf.scl_pullup_en    = GPIO_PULLUP_ENABLE;
     conf.master.clk_speed = CONFIG_LIS3DH_I2C_SPEED;
     i2c_param_config(i2c_master_port, &conf);
     return i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0);
@@ -39,11 +42,18 @@ esp_err_t LIS3DH_WriteReg(uint8_t reg, uint8_t value)
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (CONFIG_LIS3DH_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, 1);
-    i2c_master_write_byte(cmd, reg, 0);
+    i2c_master_write_byte(cmd, reg, 1);
     i2c_master_write_byte(cmd, value, 1);
     i2c_master_stop(cmd);
-    esp_err_t ret = i2c_master_cmd_begin(CONFIG_LIS3DH_I2C_PERIPH_NUM, cmd, pdMS_TO_TICKS(100));
+
+    esp_err_t ret = i2c_master_cmd_begin(I2C_NUMBER(CONFIG_LIS3DH_I2C_PERIPH_NUM), cmd, pdMS_TO_TICKS(100));
     i2c_cmd_link_delete(cmd);
+
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE("LIS3DH", "ReadReg failed (err: 0x%X)", ret);
+    }
+
     return ret;
 }
 
@@ -58,19 +68,131 @@ esp_err_t LIS3DH_WriteReg(uint8_t reg, uint8_t value)
  */
 esp_err_t LIS3DH_ReadReg(uint8_t reg, uint8_t *dataRead)
 {
+    esp_err_t ret = ESP_FAIL;
+
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (CONFIG_LIS3DH_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, 1);
     i2c_master_write_byte(cmd, reg, 1);
+    // i2c_master_stop(cmd); //we need a repeated start
+
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (CONFIG_LIS3DH_I2C_ADDRESS << 1) | I2C_MASTER_READ, 1);
     i2c_master_read_byte(cmd, dataRead, 1);
     i2c_master_stop(cmd);
-    esp_err_t ret = i2c_master_cmd_begin(CONFIG_LIS3DH_I2C_PERIPH_NUM, cmd, pdMS_TO_TICKS(100));
+    ret = i2c_master_cmd_begin(I2C_NUMBER(CONFIG_LIS3DH_I2C_PERIPH_NUM), cmd, pdMS_TO_TICKS(100));
     i2c_cmd_link_delete(cmd);
+
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE("LIS3DH", "ReadReg failed (err: 0x%X)", ret);
+    }
     return ret;
 }
 
+esp_err_t LIS3DH_ConfigureMotionDetection(uint8_t ODR, uint8_t LPen, uint8_t axisEn, uint8_t enIrqPin1, uint8_t scale,
+                                          uint8_t latch, uint8_t D4D, uint8_t threshold, uint8_t duration)
+{
+    esp_err_t ret = ESP_OK;
+
+    uint8_t ctrl1 = 0b00101111; //((ODR & 0b1111) << 4) | ((LPen & 0b1) << 3) | (axisEn & 0b111);
+    uint8_t ctrl2 = 0b00001001; // high pass settings
+    uint8_t ctrl3 = 0b01000000; //(enIrqPin1 & 0b1) << 6
+    uint8_t ctrl4 = 0b10000000; // 0x80 | ((scale & 0b11) << 4); // scale + BDU
+    uint8_t ctrl5 = 0x08;       //((latch & 0b1) << 3) | ((D4D & 0b1) << 2); //latch / 6D
+    // uint8_t ctrl6;           // not used
+    uint8_t int1cfg = 0b101010; //((axisEn & 0b001) << 1) & ((axisEn & 0b010) << 3) & ((axisEn & 0b100) << 5);
+    uint8_t int1ths = threshold & 0x7F;
+    uint8_t int1dur = duration & 0x7F;
+
+    ret |= LIS3DH_WriteReg(LIS3DH_REG_CTRL1, ctrl1);
+    vTaskDelay(pdMS_TO_TICKS(110));
+    ret |= LIS3DH_WriteReg(LIS3DH_REG_CTRL2, ctrl2); // Enable high pass filter
+    ret |= LIS3DH_WriteReg(LIS3DH_REG_CTRL3, ctrl3); // enable/disable IRQ pin for AOI1
+    ret |= LIS3DH_WriteReg(LIS3DH_REG_CTRL4, ctrl4); // set sensor scale (2g, 4g, 8g, 16g)
+    ret |= LIS3DH_WriteReg(LIS3DH_REG_CTRL5, ctrl5); // set interrupt latch and 4D/6D mode
+
+    uint8_t ref;
+    ret |= LIS3DH_ReadReg(LIS3DH_REG_REFERENCE, &ref);    // read the reference register to initialize high pass filter
+    ret |= LIS3DH_WriteReg(LIS3DH_REG_INT1_THS, int1ths); // threshold in LSB count (1LSB = scale/128)
+    ret |= LIS3DH_WriteReg(LIS3DH_REG_INT1_DUR, int1dur); // duration in 1/ODR unit
+
+    // vTaskDelay(pdMS_TO_TICKS(20)); // delay to avoid first IRQ at boot
+
+    ret |= LIS3DH_WriteReg(LIS3DH_REG_INT1_CFG, int1cfg); // OR combination of axis high
+
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE("LIS3DH", "Motion Detect Config Failed (err: 0x%X)", ret);
+    }
+    return ret;
+}
+
+// static xQueueHandle   gpio_evt_queue = NULL;
+// static void IRAM_ATTR gpio_isr_handler(void *arg)
+// {
+//     uint32_t gpio_num = (uint32_t)arg;
+//     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+// }
+
 esp_err_t LIS3DH_Init()
 {
-    esp_err_t err = i2c_master_init();
+    esp_err_t err = ESP_OK;
+
+    // initialize I2C master driver
+    err = LIS3DH_I2C_Init();
+    if (err != ESP_OK)
+    {
+        ESP_LOGE("LIS3DH", "Failed to initialize I2C");
+        return err;
+    }
+    // test communication by reading the WHO AM I register
+    uint8_t dr = 0;
+    err        = LIS3DH_ReadReg(LIS3DH_REG_WHO_AM_I, &dr);
+    if (err != ESP_OK)
+        ESP_LOGE("LIS3DH", "ReadReg failed (%d)", err);
+
+    if (dr != 0x33)
+    {
+        ESP_LOGE("LIS3DH", "Unkown LIS3DH ID (0x%02X, expected 0x33)", dr);
+        return ESP_FAIL;
+    }
+
+    //###############################################################
+    // TEST IRQ SETTINGS
+    // LIS3DH_ConfigureMotionDetection(2, 1, 0b111, 1, 1, 1, 0, 8, 1);
+    /*
+        gpio_config_t io_conf;
+        io_conf.intr_type    = GPIO_PIN_INTR_POSEDGE;
+        io_conf.pin_bit_mask = 1 << CONFIG_ELOPS_ACCELEROMETER_INT1_PIN;
+        io_conf.mode         = GPIO_MODE_INPUT;
+        io_conf.pull_up_en   = 0;
+        io_conf.pull_down_en = 0;
+        gpio_config(&io_conf);
+
+        // install gpio isr service
+        gpio_install_isr_service(0);
+        // hook isr handler for specific gpio pin
+        gpio_isr_handler_add(CONFIG_ELOPS_ACCELEROMETER_INT1_PIN, gpio_isr_handler,
+                             (void *)CONFIG_ELOPS_ACCELEROMETER_INT1_PIN);
+
+        gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+        uint32_t io_num;
+        uint32_t retry = 3000;
+        while (retry--)
+        {
+            if (xQueueReceive(gpio_evt_queue, &io_num, pdMS_TO_TICKS(10)))
+            {
+                if (gpio_get_level(CONFIG_ELOPS_ACCELEROMETER_INT1_PIN))
+                {
+                    ESP_LOGW("IRQ", "IRQ PIN HIGH");
+                }
+
+                LIS3DH_ReadReg(LIS3DH_REG_INT1_SRC, &dr);
+                ESP_LOGW("IRQ", "0x%02X", dr);
+            }
+        }
+        */
+    //###############################################################
     return err;
 }
